2cb9f6a3341cdbac0e8a59c75c1c534e55317be4
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2022-02-03 11:50:02 -0500

    String Assignment - Formatting

--- StringAssignment/src/com/hcl/stingAssignment/StringPractice.java
+++ StringAssignment/src/com/hcl/stingAssignment/StringPractice.java
@@ -3,39 +3,36 @@ package com.hcl.stingAssignment;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class TokenAssignment {
+public class StringPractice {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		String s = "";
-		
-		
+
 		System.out.println("Enter first_middle_last");
 		s = scan.nextLine();
 		s = s.replaceAll("_", ":");
 		System.out.println(s);
-		
-		
+
 		System.out.println("Enter first_middle_last");
 		s = scan.nextLine();
 		s = s.replaceAll("_", " ");
 		System.out.println(s);
-		
+
 		System.out.println("Enter string you would like to tokenize");
 		s = scan.nextLine();
 		System.out.println("Enter the delimiter you would like to use");
 		String delim = scan.nextLine();
 		StringTokenizer st = new StringTokenizer(s, delim);
-		while(st.hasMoreTokens()) {
+		while (st.hasMoreTokens()) {
 			System.out.println(st.nextElement());
 		}
-		
+
 		System.out.println("Enter first middle last");
 		s = scan.nextLine();
 		StringBuilder sb = new StringBuilder(s);
 		sb.append(" Developer");
 		sb.append(" Computer Science");
 		System.out.println(sb);
-		
 
 		scan.close();
 	}
