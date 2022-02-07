ad20cf11757e0186ee0ab17957ef4c37fb0e6662
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2022-02-07 09:26:16 -0500

    HW2 - Employee ArrayList

--- CollectionAssignment/src/main/java/com/hcl/collection/arraylist/ArrayListAndTheory.java
+++ CollectionAssignment/src/main/java/com/hcl/collection/arraylist/ArrayListAndTheory.java
@@ -1,5 +1,31 @@
 package com.hcl.collection.arraylist;
 
+import java.util.ArrayList;
+
 public class ArrayListAndTheory {
 
+	public static void main(String args[]) {
+		ArrayList<Employee> empList = new ArrayList<Employee>();
+		empList.add(new Employee(1, "John", "New York"));
+		empList.add(new Employee(2, "Jane", "New Jersey"));
+		empList.add(new Employee(3, "Jake", "New Mexico"));
+
+		for (Employee emp : empList) {
+			System.out.println(emp.id + " " + emp.name + " " + emp.city);
+		}
+
+		/**
+		 * 2.In which condition ArrayList should be used.
+		 * 
+		 * When we need constant time for search operation
+		 * 
+		 * 3.For which scenarios Map should be used.
+		 * 
+		 * When the order does not matter but data should be retrieved with a key
+		 * 
+		 * 4.For which scenarios Set should be used.
+		 * 
+		 * To have no duplicate values
+		 */
+	}
 }
---
+++ CollectionAssignment/src/main/java/com/hcl/collection/arraylist/Employee.java
@@ -0,0 +1,13 @@
+package com.hcl.collection.arraylist;
+
+public class Employee {
+	int id;
+	String name;
+	String city;
+
+	public Employee(int id, String name, String city) {
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+}
